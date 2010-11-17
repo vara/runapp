ef3bcd515b553b9090b1770a6675e14b2bc377fb
Grzegorz (vara) Warywoda <war29@wp.pl>
2010-11-17 02:42:47 +0100

    Added trace log for Printing ClassPath on start up

--- ide_projects/boot-app/scripts/posix/python/runapp.py
+++ ide_projects/boot-app/scripts/posix/python/runapp.py
@@ -360,7 +360,8 @@ def main(rawArguments):
 	globalVariables = env.getExportedVars()
 
 	if LOG.isDebug():
-
+		if LOG.isTrace():
+			LOG.trace("ClassPath: %s" % dependency )
 		LOG.debug("Exec tool: '%s'",Config.getExecTool())
 		LOG.debug("Exec path: '%s'",execPath)
 		LOG.debug("JVM Parameters: '%s'",jvmArgs)
